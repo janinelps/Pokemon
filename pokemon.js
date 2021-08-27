f2a4d44c8fcea69f0802f00b22ddc215a2338d7a
Nicolás Gonçalves Batista da Silva <nicolasgbdas@gmail.com>
2021-08-27 10:25:21 -0300

    Separa enunciado dos dados

--- pokemon.js
+++ pokemon.js
@@ -28,30 +28,3 @@ const pokemon = {
     }
 
 }
-
-
-/*
-
-  criar uma função que receba a estrutura
-  desse objeto acima e imprima na tela
-  o seguinte template:
-  
-  Nome: Pikachu - Tipo: Eletrico
-  Habilidade: Static
-  
-  Linha de evolução:
-    Pichu >> PIKACHU >> Raichu
-  
-  Atributos:
-  
-    HP: 100
-    ATK: 55 SpATK: 100
-    DEF: 34 SpDEF: 30
-    SPEED: 150
-  
-  Ataques:
-    Lv 5 - Tackle
-    Lv 9 - Thunder Wave
-    Lv 20 - Thunderbolt
-    Lv 50 - Thunder
-*/

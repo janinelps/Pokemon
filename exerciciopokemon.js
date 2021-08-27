b708c2c508214be02df581c7aaa60e658f1c2ab4
Nicolás Gonçalves Batista da Silva <nicolasgbdas@gmail.com>
2021-08-27 14:53:01 -0300

    cria função imprimir

--- exerciciopokemon.js
+++ exerciciopokemon.js
@@ -1,10 +1,10 @@
 const pokemon = require('./pokemon')
 
 function mostraDados() {
-console.log("Nome: " + pokemon.name[0].toUpperCase() + pokemon.name.substr(1, pokemon.name.length))
-console.log("Habilidade: " + pokemon.ability[0].toUpperCase() + pokemon.ability.substr(1, pokemon.ability.length))
+    console.log("Nome: " + pokemon.name[0].toUpperCase() + pokemon.name.substr(1, pokemon.name.length))
+    console.log("Habilidade: " + pokemon.ability[0].toUpperCase() + pokemon.ability.substr(1, pokemon.ability.length))
 }
-function atributosPokemon(){
+function atributosPokemon() {
     var hp = pokemon.attributes.hp
     var atk = pokemon.attributes.attack
     var spATK = pokemon.attributes.specialAttack
@@ -12,15 +12,19 @@ function atributosPokemon(){
     var spDEF = pokemon.attributes.specialDefense
     var speed = pokemon.attributes.speed
 
-    return console.log("HP: " + hp + "\nATK: " + atk + " SpATK: " + spATK+ 
-    "\nDEF: " + def + " SpDEF: " + spDEF + "\nSPEED: " + speed)
-
-   /*    return console.log("HP: " + pokemon.attributes.hp + "\nATK: " +  pokemon.attributes.attack + 
-    "  SpATK: " +  pokemon.attributes.specialAttack + "\nDEF: " + pokemon.attributes.defense +
-    "  SpDEF: " +  pokemon.attributes.specialDefense + "\nSPEED: " + pokemon.attributes.speed)
-*/
+    return console.log("HP: " + hp + "\nATK: " + atk + " SpATK: " + spATK +
+        "\nDEF: " + def + " SpDEF: " + spDEF + "\nSPEED: " + speed)
+    /*    return console.log("HP: " + pokemon.attributes.hp + "\nATK: " +  pokemon.attributes.attack + 
+     "  SpATK: " +  pokemon.attributes.specialAttack + "\nDEF: " + pokemon.attributes.defense +
+     "  SpDEF: " +  pokemon.attributes.specialDefense + "\nSPEED: " + pokemon.attributes.speed)
+ */
 }
-atributosPokemon()
+function imprimir() {
+    return mostraDados(), 
+    atributosPokemon()
+}
+imprimir()
+
 /*
 Função 1: Manipula uma string para deixar palavras ou letras maiusculas
 
